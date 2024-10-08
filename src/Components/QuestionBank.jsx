75f740a3318c1522d27b85af9bea70ce24504b8e
rani2425 <shindevandana852@gmail.com>
2024-10-08 11:44:28 +0530

    Initial commit

---
+++ src/Components/QuestionBank.jsx
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the primary purpose of React.js?",
+        options: ["Backend", "UI", "Mobile", "Database"],
+        answer: "UI",
+    },
+    {
+        id: 2,
+        question: "What is JSX in React?",
+        options: ["Syntax", "Language", "Framework", "Library"],
+        answer: "Syntax",
+    },
+    {
+        id: 3,
+        question: "Which method is used to update the state in React?",
+        options: ["setState", "updateState", "changeState", "modifyState"],
+        answer: "setState"
+    },
+    {
+        id: 4,
+        question: "What is used to pass data to child components in React?",
+        options: ["State", "Props", "Variables", "Hooks"],
+        answer: "Props"
+    },
+    {
+        id: 5,
+        question: "Which hook is used to manage side effects in React?",
+        options: ["useEffect", "useState", "useReducer", "useMemo"],
+        answer: "useEffect"
+    },
+]
+
+export default qBank;
